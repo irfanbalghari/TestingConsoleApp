dcdb7e9b792ffd2e36ef8050988dad187fe679a9
irfanullah <=>
2023-06-19 12:09:59 +0500

    breaking changes algo

--- DiagonalSum.cs
+++ DiagonalSum.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace TestingConsoleApp
 {
-	internal class DiagonalSum
+	public class DiagonalSum
 	{
 		public DiagonalSum()
 		{
--- Program.cs
+++ Program.cs
@@ -3,16 +3,15 @@ using System.Collections.Generic;
 
 namespace TestingConsoleApp
 {
-	public class Program
-	{
+    public class Program
+    {
 
-		static void Main(string[] args)
-		{
+        static void Main(string[] args)
+        {
+            // modiler_fizzbuzz.fizzBuzz(15);
+            // RomanNumerical.Parse("I");
 
-			// modiler_fizzbuzz.fizzBuzz(15);
-			// RomanNumerical.Parse("I");
-
-			/* Delegate example 
+            /* Delegate example 
 			var video = new Video() { Title = "End 1" };
 			var VideoEncoder = new VideoEncoder(); // publisher
 			var mailService = new MailService(); // subscriber
@@ -21,31 +20,42 @@ namespace TestingConsoleApp
 			VideoEncoder.VideoEncoded += messageService.Message;
 			VideoEncoder.Encode(video);
 			*/
-			// uses boxing DateTime timeValue = Convert.ToDateTime("01:00 PM");
-			DateTime timeValue = DateTime.Parse("01:00 PM");
-			Console.WriteLine(timeValue.ToString("HH:mm"));
-			List<int> a = new()
-			{
-				7,
-				71,
-				67,
-			};
-			List<int> b = new()
-			{
-				8,
-				71,
-				99,
-			};
-			//Console.WriteLine("result: " + GradeRounding.gradingStudents(value));
-			//Console.WriteLine("result: " + SecoundHighest.getSecondHighest(a));
 
-			var result = compareTriplets.CompareTriplets(a, b);
-			result.ForEach(x => Console.WriteLine("result: " + x));
+            // uses boxing DateTime timeValue = Convert.ToDateTime("01:00 PM");
+            //	DateTime timeValue = DateTime.Parse("01:00 PM");
+            //Console.WriteLine(timeValue.ToString("HH:mm"));
 
-			//DependencyInversionPrinciple.getStarted();
-		}
+            List<int> a = new()
+            {
+                7,
+                71,
+                67,
+            };
+            List<int> b = new()
+            {
+              8,
+                71,
+                99,
+            };
+            //Console.WriteLine("result: " + GradeRounding.gradingStudents(value));
+            //Console.WriteLine("result: " + SecoundHighest.getSecondHighest(a));
 
-	}
+            //	var result = compareTriplets.CompareTriplets(a, b);
+            //	result.ForEach(x => Console.WriteLine("result: " + x));
+
+            //DependencyInversionPrinciple.getStarted();
+
+
+            List<int> n = new()
+            {
+                10, 5, 20, 20, 4, 5, 2, 25, 1
+            };
+            var result = breakingChangesInResult.breakingRecords(n);
+            Console.WriteLine(result);
+
+        }
+
+    }
 }
 
 
